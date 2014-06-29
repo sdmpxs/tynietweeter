a361d332072d8f8b9ec2a92493429276f4dd06d1
sdmpxs <shane@marriott.gg>
2014-06-29 18:45:32 +0100

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,1 @@
+javascript:if(navigator.userAgent.indexOf('Safari')%20>=%200){Q=getSelection();}else{Q=document.selection?document.selection.createRange().text:document.getSelection();}location.href='http://tynie.net/maketynie.php?remote='+encodeURIComponent(Q)+''+encodeURIComponent(location.href);
